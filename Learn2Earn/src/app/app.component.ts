6052567d9c70828ff7eb502c9317fd614373dda3
Team 4 <team4@azubihackathon.fiduciagad.de>
2023-01-26 11:45:49 +0100

    added routing

--- Learn2Earn/src/app/app.component.ts
+++ Learn2Earn/src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {Router} from '@angular/router';
+
+
+interface Site {
+  path: string;
+  name: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -8,10 +15,19 @@ import {HttpClient} from "@angular/common/http";
 })
 export class AppComponent {
   title = 'Learn2Earn';
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   test() {
     this.http.get<string>('http://localhost:1234/listUser').subscribe(value => console.log(value));
 
+  }
+
+  sites: Site[] = [
+    {path: 'finance', name: 'Finanzen'},
+    {path: 'learning', name: 'Lernen'},
+  ]
+  selectedSite: Site = this.sites[0];
+  switchSite() {
+    this.router.navigate([this.selectedSite.path]);
   }
 }
--- Learn2Earn/src/app/app.module.ts
+++ Learn2Earn/src/app/app.module.ts
@@ -4,15 +4,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from "@angular/common/http";
+import { LearningComponent } from './learning/learning.component';
+import { FinanceComponent } from './finance/finance.component';
+import {FormsModule} from "@angular/forms";
+import {RouterModule} from "@angular/router";
+import {MatSelectModule} from "@angular/material/select";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LearningComponent,
+    FinanceComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {path: '', component: FinanceComponent},
+      {path: 'finance', component: FinanceComponent},
+      {path: 'learning', component: LearningComponent},
+    ]),
+    MatSelectModule,
     HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
